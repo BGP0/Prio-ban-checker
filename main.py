04d0e34e8f8f7ded2d5426b704b7eabdff05b6da
Your Name <you@example.com>
2022-08-13 21:46:59 +0100

    rewrite

--- main.py
+++ main.py
@@ -1,83 +1,143 @@
-import asyncio
-import aiohttp
-import time
 from colorama import Fore
+import threading
+import requests
+import time
 
-# - Settings - ########
-outputUnbanList = False
-outputBanList   = False
-useInstantMode  = False
-delay           = 1.5        #will be ignored if useInstantMode is True
-debug           = False
-#######################
+usernames = [item.replace("\n", "") for item in open('usernames.txt', 'r').readlines()]
+proxies = [item.replace("\n", "") for item in open('proxies.txt', 'r').readlines()]
+stats = {
+    "Total": 0,
+    "Banned": [],
+    "Unbanned": [],
+    "Errors": [] # If program breaks or they change their name/delete acc
+}
 
-# Program Data
-unbanList = []
-banList   = []
-usernames = [item.replace("\n", "") for item in open('debugnames.txt', 'r').readlines()] if debug else [item.replace("\n", "") for item in open('usernames.txt', 'r').readlines()]
+# Very fast, removes about 15% of my list from webshare.io
+def check_proxy(proxy):
+    req = requests.post(
+        "https://shop.2b2t.org/checkout/packages/add/1994962/single?ign=CAEC64",
+        allow_redirects=False,
+        proxies={
+            "http": proxy,
+            "https": proxy
+        }
+    )
 
-def output(username, request):
-    output = f"{Fore.LIGHTBLACK_EX}<{Fore.LIGHTBLUE_EX}{username}{Fore.LIGHTBLACK_EX}> " + (" " * (16 - len(username)))
-
-    if request.headers["Connection"] != "keep-alive":
-        print(Fore.WHITE + f"RATE LIMITED: {username} (Connection: {request.headers['Connection']})")
-        return
-        
-    if "Set-Cookie" in request.headers:
-        response = request.headers["Set-Cookie"].split("; ")[0][8:]
-        
-        # TODO: Add a switch statement but need to wait for python 3.10
-        if (response == "XRxlbOYKOzX5HYSsk7VO79kWPpwubYetzyPeyIphpZPxM%2BTo3%2BDuWHTzXALpQCz%2FyK8QCd0VpduRCEow0JIFDg%3D%3D"):
-            output += Fore.LIGHTYELLOW_EX + "Unable to find a player with that username"
-        elif (response == "AeSOVxjOOOMBhHAGYvcmCdJEE6pUQYonWMSJbkGLdUV2oNeagFeBuFUSV3NdTnIa"):
-            output += Fore.YELLOW + "That is not a valid username"
-        elif (response == "deleted"):
-            output += Fore.RED + "That user is banned"
-            banList.append(username)
+    try:
+        if "XRxlbOYKOzX5HYSsk7VO72KxURUxqkzYCSTxTat" in req.headers["Set-Cookie"].split("; ")[0]:
+            print(proxy + " is working")
         else:
-            output += Fore.GREEN + "Unbanned"
-            unbanList.append(username)
+            print(req.headers["Set-Cookie"].split("; ")[0])
+    except:
+        print(proxy + " is not working")
+        proxies.remove(proxy)
+
+def check_proxies():
+    threads = []
+
+    for proxy in proxies:
+        thread = threading.Thread(target=check_proxy, args=(proxy,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    print(f"There are {len(proxies)} working proxies")
+
+def output(username, result):
+    stats["Total"] += 1
+
+    if "buycraft_basket" in result:
+        res = Fore.GREEN + username + " is unbanned"
+        stats["Unbanned"] += [username]
+    elif "XRxlbOYKOzX5HYSsk7VO72KxURUxqkzYCSTxTat" in result:
+        res = Fore.LIGHTRED_EX + username + " is banned"
+        stats["Banned"] += [username]
+    elif result == "message=XRxlbOYKOzX5HYSsk7VO79kWPpwubYetzyPeyIphpZPxM%2BTo3%2BDuWHTzXALpQCz%2FyK8QCd0VpduRCEow0JIFDg%3D%3D":
+        res = Fore.BLUE + username + " is not found"
+        stats["Errors"] += [username]
+    elif result == "message=AeSOVxjOOOMBhHAGYvcmCdJEE6pUQYonWMSJbkGLdUV2oNeagFeBuFUSV3NdTnIa":
+        res = Fore.BLUE + username + " is invalid"
+        stats["Errors"] += [username]
+    elif result == "message=XRxlbOYKOzX5HYSsk7VO79kWPpwubYetzyPeyIphpZOryToGcxK9AL6pF0TwQ1EZ":
+        res = Fore.BLUE + username + " is deleted"
+        stats["Errors"] += [username]
     else:
-        output += Fore.GREEN + "Unbanned"
-        unbanList.append(username)
-
-    print(output)
-
-# Different modes
-if useInstantMode:
-    # Checks a given username
-    async def check(username, session):
-        async with session.get(f"https://shop.2b2t.org/checkout/packages/add/1994771/subscription?ign={username}") as request:
-            output(username, request)
-
-    # Asynchronously check the status of each username from a list
-    async def checkAll(usernames):
-        async with aiohttp.ClientSession() as session:
-            tasks = []
-
-            for username in usernames:
-                tasks.append(asyncio.ensure_future(check(username, session)))
-            
-            await asyncio.gather(*tasks)
-
-
-    # Start the program
-    start_time = time.time()
-    asyncio.run(checkAll(usernames))
-else:
-    async def checkAll():
-        for username in usernames:
-            time.sleep(delay)
-            async with aiohttp.request("GET", f"http://shop.2b2t.org/?ign={username}") as request:
-                output(username, request)
+        stats["Errors"] += [username]
+        res = "Unexpected result" + result + username
     
-    # Start the program
-    start_time = time.time()
-    asyncio.run(checkAll())
+    print(f"{res}{Fore.RESET} ({stats['Total']}/{len(usernames)})")
 
-# Additional outputs, if configured
-if (outputUnbanList): print(Fore.GREEN + "\n".join(unbanList))
-if   (outputBanList): print(Fore.RED   + "\n".join(  banList))
+def check(username, proxy):
+    if proxy != None:
+        req = requests.post(
+            "https://shop.2b2t.org/checkout/packages/add/1994962/single?ign=" + username,
+            allow_redirects=False,
+            proxies={
+                "http": proxy,
+                "https": proxy
+            }
+        )
+    else:
+        req = requests.post(
+            "https://shop.2b2t.org/checkout/packages/add/1994962/single?ign=" + username,
+            allow_redirects=False
+        )
 
-# Resets the terminal color and outputs time taken
-print(Fore.RESET + f"Checked all usernames in {round(time.time() - start_time, 2)} seconds!")
+    if req.headers.__contains__("Retry-After"):
+        print("RATE LIMITED FOR " + req.headers["Retry-After"] + "s")
+        stats["Errors"] += [username]
+    elif req.headers.__contains__("Set-Cookie"):
+        output(username, req.headers["Set-Cookie"].split("; ")[0])
+    else:
+        stats["Errors"] += [username]
+        stats["Total"] += 1
+        print("Proxy error ?", username, proxy, req.status_code)
+
+def check_thread(id, dim):
+    proxy = proxies[id]
+
+    for i in range(id, len(usernames), dim):
+        check(usernames[i], proxy)
+        time.sleep(1.5)
+
+def check_all():
+    if len(proxies) > 0:
+        threads = []
+        dim = len(proxies)
+
+        for i in range(dim):
+            thread = threading.Thread(target=check_thread, args=(i, dim))
+            thread.start()
+            threads.append(thread)
+        
+        print("Started all threads")
+
+        for thread in threads:
+            thread.join()
+    else:
+        for username in usernames:
+            check(username)
+            time.sleep(1.5)
+        
+    print("Finished")
+
+def save():
+    with open("logs.txt", "a+") as f:
+        text = ''
+        
+        for i in ["Banned", "Unbanned", "Errors"]:
+            text += f"\n\n{i}:\n\n"
+            for j in stats[i]:
+                text += j + '\n'
+
+        f.write(text)
+
+start_time = time.time()
+check_proxies()
+print(f"It took {time.time() - start_time}s to check proxies")
+start_time = time.time()
+check_all()
+print(f"It took {time.time() - start_time}s to finish")
+save()
